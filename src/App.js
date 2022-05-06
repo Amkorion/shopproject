e37d20b5abeacce84548f53031b0594f91d7f869
Oleksandr Moroz <morozlex@gmail.com>
2022-05-06 14:28:08 +0300

    visual

--- src/App.js
+++ src/App.js
@@ -1,41 +1,39 @@
+import "./categories.styles.scss";
+import CategoryItem from "./components/category-item/category-item.component.jsx";
+
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Капелюхи",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "Верхній одяг",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "Взуття",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "Для Жінок",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "Для Чоловіків",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h2>Капелюхи</h2>
-          <p>Перейти до магазину</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h2>Куртки</h2>
-          <p>Перейти до магазину</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h2>Взуття</h2>
-          <p>Перейти до магазину</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h2>Жінкам</h2>
-          <p>Перейти до магазину</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img/> */}
-        <div className="category-body-container">
-          <h2>Чоловікам</h2>
-          <p>Перейти до магазину</p>
-        </div>
-      </div>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
   );
 };
