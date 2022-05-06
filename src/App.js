f476134eb116dea021261880590c746e7d9d8afd
Oleksandr Moroz <morozlex@gmail.com>
2022-05-06 15:05:16 +0300

    uprate directory

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-import "./categories.styles.scss";
-import CategoryItem from "./components/category-item/category-item.component.jsx";
+import Directory from "./components/Directory/Directory";
+import CategoriesData from "./components/categories-data/categories-data.jsx";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "Капелюхи",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "Верхній одяг",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "Взуття",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "Для Жінок",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "Для Чоловіків",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Directory Categories={CategoriesData} />;
 };
 
 export default App;
---
+++ src/components/categories-data/categories-data.jsx
@@ -0,0 +1,29 @@
+const CategoriesData = [
+  {
+    id: 1,
+    title: "Капелюхи",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+  },
+  {
+    id: 2,
+    title: "Верхній одяг",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+  },
+  {
+    id: 3,
+    title: "Взуття",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+  },
+  {
+    id: 4,
+    title: "Для Жінок",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+  },
+  {
+    id: 5,
+    title: "Для Чоловіків",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+  },
+];
+
+export default CategoriesData;
