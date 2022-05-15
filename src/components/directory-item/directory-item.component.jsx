2feae0d0eabc2c43059c3db046b6cb564f0844e6
Oleksandr Moroz <morozlex@gmail.com>
2022-05-15 20:03:25 +0300

    lets try here

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { DirectiryItemContainer, BackgroundImage, Body } from "./directory-item.styles.jsx";
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
+  const { imageUrl, title, route } = category;
+
+  const navigation = useNavigate();
+  const navigationHandler = () => navigation(route);
+
   return (
-    <DirectiryItemContainer>
+    <DirectiryItemContainer onClick={navigationHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
